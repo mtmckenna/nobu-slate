b05a5d69c44b37c9ed96dc3e330d242a1e298bf9
Matt McKenna <matt@mtmckenna.com>
2012-12-25 23:48:00 -0600

    Adding application tests. New icon. Cleaned up storyboard for iPhone.

---
+++ NoBuSlateApplicationTests/NoBuSlateApplicationTests.h
@@ -0,0 +1,13 @@
+//
+//  NoBuSlateApplicationTests.h
+//  NoBuSlateApplicationTests
+//
+//  Created by Matthew McKenna on 12/25/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NoBuSlateApplicationTests : SenTestCase
+
+@end
---
+++ NoBuSlateApplicationTests/NoBuSlateApplicationTests.m
@@ -0,0 +1,32 @@
+//
+//  NoBuSlateApplicationTests.m
+//  NoBuSlateApplicationTests
+//
+//  Created by Matthew McKenna on 12/25/12.
+//
+//
+
+#import "NoBuSlateApplicationTests.h"
+
+@implementation NoBuSlateApplicationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NoBuSlateApplicationTests");
+}
+
+@end
